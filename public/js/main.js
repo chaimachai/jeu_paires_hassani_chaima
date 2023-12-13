4226ba0bf8182421b5be35eed8776d78a4c12680
chaima <hassanichaima@outlook.be>
2023-12-13 12:27:04 +0100

    animation indice

--- public/js/main.js
+++ public/js/main.js
@@ -11,19 +11,19 @@ let btn_no = document.querySelector("#btn_no");
 let jeu = document.querySelector(".jeu");
 let tableau_score = document.querySelector(".tableau_score");
 let clone = document.querySelector(".clone");
-let affichage2 = document.querySelector(".affichage_2")
-let continu = true;
+let affichage2 = document.querySelector(".affichage_2");
+let maisons = ["gryffondor","serpentard","poufsouffle","serdaigle"];
+let niveaux = ["facile","moyen","difficile"];
 let pseudo;
-let game;
+let maison;
+let niveau;
+let les_src;
+let continu = true;
 let sec = 0;
 let min = 0;
 let heure = 0;
 let count = 0;
 let echec = 0;
-let maison;
-let maisons = ["gryffondor","serpentard","poufsouffle","serdaigle"];
-let niveau;
-let niveaux = ["facile","moyen","difficile"];
 
 let joueur = {
     maison: maison,
@@ -66,15 +66,14 @@ btn_yes.addEventListener("click",()=>{
     score.classList.add("none");
     jeu.classList.remove("none");
     setTimeout(()=>{
-        partie()
+        partie();
     },1000)
 })
 
 let melange = () => {
-    let les_src;
     if (niveau == "facile"){
         les_src = ["./public/img/carte_1.jpg", "./public/img/carte_1.jpg", "./public/img/carte_2.jpg" ,"./public/img/carte_2.jpg" ,"./public/img/carte_3.jpg","./public/img/carte_3.jpg"];
-        cartes[0].parentElement.style = "grid-template-columns: repeat(3,1fr); gap: 4rem"
+        cartes[0].parentElement.style = "grid-template-columns: repeat(3,1fr); gap: 4rem";
     }else if (niveau == "moyen"){
         les_src = ["./public/img/carte_1.jpg", "./public/img/carte_1.jpg", "./public/img/carte_2.jpg" ,"./public/img/carte_2.jpg" ,"./public/img/carte_3.jpg","./public/img/carte_3.jpg","./public/img/carte_4.jpg","./public/img/carte_4.jpg"];
         for(let i=0; i<2; i++){
@@ -100,7 +99,7 @@ let melange = () => {
             div.append(img_recto,img_verso);
             cartes[0].parentElement.appendChild(div);
         }
-        cartes[0].parentElement.style = "grid-template-columns: repeat(5,1fr); gap: 1rem"
+        cartes[0].parentElement.style = "grid-template-columns: repeat(5,1fr); gap: 1rem";
     }
     recto = document.querySelectorAll(".recto");
     verso = document.querySelectorAll(".verso");
@@ -124,7 +123,7 @@ let retourner = (e)=>{
         },250)
         count += 1;
         if (count == 2){
-            continu = false
+            continu = false;
             setTimeout(()=>{
                 let test = document.querySelectorAll(".test");
                 if (test[0].src == test[1].src){
@@ -132,11 +131,11 @@ let retourner = (e)=>{
                     test[1].classList.add("find");
                     echec = 0;
                     if (document.querySelectorAll(".find").length == cartes.length){
-                        score.querySelector(".question").innerHTML = `Veux-tu jouer une nouvelle partie en mode ${niveau} ${pseudo} ?`
+                        score.querySelector(".question").innerHTML = `Veux-tu jouer une nouvelle partie en mode ${niveau} ${pseudo} ?`;
                         if(tableau_score.querySelectorAll("h2").length > 40){
                             tableau_score.querySelectorAll("div").forEach(elements => {
                                 Array.from(elements.children).forEach(element => {
-                                    elements.removeChild(element)
+                                    elements.removeChild(element);
                                 })
                             })
                         }
@@ -155,9 +154,9 @@ let retourner = (e)=>{
                         score.classList.remove("none");
                         jeu.classList.add("none");
                         let carte_supp = document.querySelectorAll('.supp');
-                        let section_cartes = document.querySelector(".section_cartes")
+                        let section_cartes = document.querySelector(".section_cartes");
                         carte_supp.forEach(element =>{
-                            section_cartes.removeChild(element)
+                            section_cartes.removeChild(element);
                         })
                     }
                 }else{
@@ -179,21 +178,21 @@ let retourner = (e)=>{
                 test[1].classList.remove("test");
                 test[0].parentElement.classList.remove("animation");
                 test[1].parentElement.classList.remove("animation");
-                count = 0
-                continu = true
+                count = 0;
+                continu = true;
             },500)
         }
     }
 }
 setInterval(()=>{
-    sec++
+    sec++;
     if (sec == 60){
         sec = 0;
-        min++
+        min++;
     }
     if(min == 60){
         min = 0;
-        heure++
+        heure++;
     }
     (heure == 0)? chrono.innerHTML = `${min} : ${sec}` : chrono.innerHTML = `${heure} : ${min} : ${sec}`
 },1000)
@@ -202,8 +201,8 @@ let partie = () =>{
     min = 0;
     heure = 0;
     count = 0;
-    melange()
-    clone.innerHTML = tableau_score.innerHTML
+    melange();
+    clone.innerHTML = tableau_score.innerHTML;
     verso.forEach(element => {
         element.src = "./public/img/" + maison + ".png";
         element.classList.remove("none");
@@ -213,6 +212,6 @@ let partie = () =>{
         element.classList.add("none");
     })
     verso.forEach(element => {
-        element.addEventListener("click",retourner)
+        element.addEventListener("click",retourner);
     })
 }
